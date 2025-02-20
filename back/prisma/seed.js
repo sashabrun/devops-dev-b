f2d89b43eb6868f23cfd3a4a5f1330e0a6afca89
Sasha <sasha.brun@ynov.com>
2025-02-20 11:00:05 +0100

    add_docker_add_userAdmin

---
+++ back/prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const admin = await prisma.user.create({
+        data: {
+            username: 'admin',
+            firstName: 'Admin',
+            lastName: 'User',
+            password: await bcrypt.hash('adminpassword', 10),
+            isAdmin: true,
+        },
+    });
+
+    console.log('Utilisateur admin créé:', admin);
+}
+
+main()
+    .catch(e => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
